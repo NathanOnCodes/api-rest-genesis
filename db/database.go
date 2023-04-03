ab317dcb6f2195b23a97a3500e80600b547e1091
NathanCavalcanteFerreira <contatonathancf@gmail.com>
2023-04-02 21:20:37 -0300

    troca de host

--- db/database.go
+++ db/database.go
@@ -10,7 +10,7 @@ import (
 var DB *gorm.DB
 
 func InitDB() *gorm.DB {
-	dns := "admin:admin@tcp(db:3306)/db?charset=utf8mb4&parseTime=True&loc=Local"
+	dns := "admin:admin@tcp(127.0.0.1:3306)/db?charset=utf8mb4&parseTime=True&loc=Local"
 	time.Sleep(2 * time.Second)
 
 	for i := 1; i <= 3; i++ {
